71df803c9a9b5bd46b0e23735fad9e36af8e4e39
연욱 정 <vire3064@gmail.com>
2017-12-28 22:02:51 +0900

    Revert "Revert "1. public void SetFormManager -> internal void SetFormManager로 변경""
    This reverts commit ebc88c0df1bfac448e16da3a803199f2f243563e.

---
+++ MoneyTaker/Forms/Setting/TabSettingForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoneyTaker
+{
+    public partial class TabSettingForm : Form
+    {
+        private FormExchangeManager formManager;
+
+        public TabSettingForm()
+        {
+            InitializeComponent();
+        }
+
+        internal void SetFormManager(FormExchangeManager formManager)
+        {
+            this.formManager = formManager;
+        }
+
+        private async void TabSettingForm_LoadAsync(object sender, EventArgs e)
+        {
+            for (Width = 0; Width < 700; Width += 50)
+                await Task.Delay(1);
+        }
+
+        private void btnSettingSetting_Click(object sender, EventArgs e)
+        {
+            formManager.ShowTabBasicForm();
+        }
+    }
+}
