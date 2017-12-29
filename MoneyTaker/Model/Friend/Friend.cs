3df9d0ac3bfdd42b82caa26e42856ce566bf46e5
김두상 <ds0213@nate.com>
2017-12-29 20:27:01 +0900

    모델화

---
+++ MoneyTaker/Model/Friend/Friend.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Friend
+    {
+        String name;
+        String email;
+        String group;
+        int credibility;
+
+        public Friend(String name, String email,String group, int credibility)
+        {
+            this.name = name;
+            this.email = email;
+            this.group = group;
+            this.credibility = credibility;
+        }
+        public String getName()
+        {
+            return name;
+        }
+
+        public String getEmail()
+        {
+            return email;
+        }
+
+        public String getGroup()
+        {
+            return group;
+        }
+
+        public int getCredibility()
+        {
+            return credibility;
+        }
+    }
+
